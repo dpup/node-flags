738213dcde7c117621ef60b9a74d4258822b298e
pupius <dan.pupius@gmail.com>
2011-03-25 12:11:50 -0700

    More flags changes

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,30 @@
+/**
+ * @fileoverview Basic example of flag usage.
+ *
+ * Try:
+ *   node example.js --name='Dan Pupius' --age=31 --height=1.85 \
+ *     --pets=Ada,Oscar --hobby=triathlon --hobby=photography
+ *
+ * @author dan@pupi.us (Daniel Pupius)
+ */
 
 var flags = require('flags');
 
-flags.defineString('str', 'hello');
-flags.defineInteger('int', 20);
-flags.defineNumber('num', 1.234);
-
+flags.defineString('name', 'Billy Noone', 'Your name');
+flags.defineInteger('age', 21, 'Your age in whole years');
+flags.defineNumber('height', 1.80, 'Your height in meters');
+flags.defineStringList('pets', []);
+flags.defineMultiString('hobby', []);
 
 flags.parse();
 
-flags.help();
+// ====
+
+var info = [];
+info.push('Name : ' + flags.get('name'));
+info.push('Age : ' + flags.get('age'));
+info.push('Height : ' + flags.get('height') + '"');
+info.push('Pets : ' + flags.get('pets').join(', '));
+info.push('Hobbies : \n  ' + flags.get('hobby').join('\n  '));
+
+console.log(info.join('\n'));
